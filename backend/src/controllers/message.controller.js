606a0bd669f30b1bfe037a760de655f0826c122a
Lavanya Rajeswari <saridelavanyarajeswari@gmail.com>
2026-01-08 22:22:39 +0530

    Message Routes Updated

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -39,6 +39,19 @@ export const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
+    if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
+    }
+    if (senderId.equals(receiverId)) {
+      return res
+        .status(400)
+        .json({ message: "Cannot send messages to yourself." });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found." });
+    }
+
     let imageUrl;
     if (image) {
       const uploadResponse = await cloudinary.uploader.upload(image);
@@ -76,10 +89,12 @@ export const getChatPartners = async (req, res) => {
         )
       ),
     ];
-    const chatPartners = await User.find({_id: {$in:chatPartnerIds}}).select("-password")
-    res.status(200).json(chatPartners)
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select("-password");
+    res.status(200).json(chatPartners);
   } catch (error) {
-     console.log("Error in getChatPartners Controller : ", error.message);
+    console.log("Error in getChatPartners Controller : ", error.message);
     res.status(500).json({ message: "Internal Server error" });
   }
 };
