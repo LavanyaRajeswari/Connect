8dfb56ceb973e932f6fd2066c77b1d5e13e9e5cc
Lavanya Rajeswari <saridelavanyarajeswari@gmail.com>
2026-01-12 17:21:39 +0530

    signup frontend implementation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,38 +1,19 @@
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        border: "border 4s linear infinate",
       },
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: "1rem",
-          sm: "2rem",
-          lg: "3rem",
-          xl: "4rem",
-          "2xl": "4rem",
-          "3xl": "5rem",
-        },
-      },
-
-      extend: {
-        screens: {
-          "4k": "1980px",
+      keyframes: {
+        border: {
+          to: { "--border-angle": "360deg" },
         },
       },
     },
   },
-  plugins: [
-    daisyui
-  ],
-}
+  plugins: [daisyui],
+};
